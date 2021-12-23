06b61c21b03f9c1498fd59473fb3a22aa0d068b7
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2021-12-23 10:14:13 -0300

    feat: Configure graphql and koa

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,46 @@
-console.log('listening yeah');
+import Koa from 'koa';
+import Router from 'koa-router';
+import { graphqlHTTP, OptionsData } from 'koa-graphql';
+import { GraphQLError } from 'graphql';
+import koaPlayground from 'graphql-playground-middleware-koa';
+import schema from './graphql/schema';
 
-console.log('here;');
+const router = new Router();
+const app = new Koa();
+
+router.get('/', async (ctx) => {
+  ctx.body = 'first router';
+});
+
+const graphqlSettingsPerReq = async () => {
+  return {
+    graphiql: process.env.NODE_ENV !== 'production',
+    schema,
+    customFormatErrorFn: (error: GraphQLError) => {
+      console.log(error.message);
+      console.log(error.locations);
+      console.log(error.stack);
+
+      return {
+        message: error.message,
+        locations: error.locations,
+        stack: error.stack,
+      };
+    },
+  } as OptionsData;
+};
+
+const graphqlServer = graphqlHTTP(graphqlSettingsPerReq);
+
+router.all('/graphql', graphqlServer);
+
+router.all(
+  '/graphiql',
+  koaPlayground({
+    endpoint: '/graphql',
+  })
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+export default app;
